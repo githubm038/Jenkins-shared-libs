eec2b9f96a3fba13d9257d092bd115359d7c514a
githubm038 <76935536+githubm038@users.noreply.github.com>
2024-12-12 15:53:29 +0530

    Create docker_push.groovy

---
+++ vars/docker_push.groovy
@@ -0,0 +1,7 @@
+def call (String Project, String ImageTag, String DockerhubUser){
+  echo 'Pushing an image to DockerHub...'
+  withCredentials([usernamePassword(credentialsId:"DockerHubCred",
+  passwordVariable:"dockerHubPass",usernameVariable:"dockerHubUser")]){
+  sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"}
+  sh "docker push ${env.dockerHubUser}/notes-app:latest"
+}
