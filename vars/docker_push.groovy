bedaa6a48a63bcf22fd8ff88ab59ba2349405080
githubm038 <76935536+githubm038@users.noreply.github.com>
2024-12-12 17:41:10 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,8 +1,6 @@
 def call (String Project, String ImageTag, String dockerhubuser){
-  withCredentials([usernamePassword(credentialsId:"DockerHubCred",
-  passwordVariable:"dockerHubPass",usernameVariable:"dockerHubUser")]){
+  withCredentials([usernamePassword(credentialsId:"dockerHubCred", passwordVariable:"dockerHubPass",usernameVariable:"dockerHubUser")]){
     sh "docker login -u ${dockerhubuser} -p ${dockerHubPass}"
-    sh "docker image tag ${Project}:${ImageTag} ${dockerhubuser}/${Project}:${ImageTag}"
   }
   sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
 }
